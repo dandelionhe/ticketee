d1b3b5e2d599eab05b51848b68f864f20fc522d8
dandelionhe <heyongzheng84@gmail.com>
2017-07-16 15:26:38 +0800

    add full title function

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,14 +1,28 @@
 require "rails_helper"
 
 RSpec.feature 'Users can create new project' do
-	scenario 'with valid attributes' do
+	before do
 		visit '/'
 		click_link 'New Project'
+	end
+
+	scenario 'with valid attributes' do
+
 
 		fill_in 'Name',with:'Sublime Text 3'
 		fill_in 'Description',with:'A text editor for everyone.'
 		click_button 'Create Project'
 
 		expect(page).to have_content 'Project has been created.'
+
+		project = Project.find_by(name:'Sublime Text 3')
+		expect(page.current_url).to eq project_url(project)
+
+		title = 'Sublime Text 3 - Projects - Ticketee'
+		expect(page).to have_title title
+	end
+
+	scenario 'scenario description' do
+		
 	end
 end
