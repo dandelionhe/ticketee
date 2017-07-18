2069e03cb6e45abc980cc499ed700c4b07a06d00
dandelionhe <heyongzheng84@gmail.com>
2017-07-18 09:32:56 +0800

    implement  creating tickets for a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
 	validates :name,presence:true
-	
+	has_many :tickets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716033056) do
+ActiveRecord::Schema.define(version: 20170718005213) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tickets_on_project_id"
   end
 
 end
